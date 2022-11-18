08c894bbde1445eda17bd8b00220a02c162cb6d4
skyArony <1450872874@qq.com>
2022-11-19 00:35:46 +0800

    impr: 将 x.x.x 格式的镜像版本号固定为 latest

--- Node/build.sh
+++ Node/build.sh
@@ -1,5 +1,5 @@
 # 编译镜像
-docker build . -t env_dev_node:1.0.0 -f AMD.Dockerfile
+docker build . -t env_dev_node:latest -f AMD.Dockerfile
 
 # 删除多余镜像
 docker images | grep "none" | awk '{print $3}'| xargs -t -n1 docker rmi
